9db6f2871c09c1528c3812547d64e4b0dd63553c
Dmitry Shibanov <shibanov-1997@inbox.ru>
2019-12-13 13:46:03 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Дмитрий Шибанов on 13/12/2019.
+//  Copyright © 2019 Dmitry Shibanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
